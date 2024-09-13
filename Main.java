acc8668fdc4276c078150279f9af5699191d40da
BrianZ95 <brianz0905@gmail.com>
2024-09-13 13:07:33 +0000

    Main.java

--- Main.java
+++ Main.java
@@ -6,14 +6,30 @@ public class Main
         System.out.println("Welome to my calculator!");
         System.out.println("Enter your first number.");
         Scanner s = new Scanner(System.in);
-        int first = s.nextInt();
+        double first = s.nextDouble();
         System.out.println("What operation would you like to do?");
         String operation = s.next();
         System.out.println("Enter your second number.");
-        int second = s.nextInt();
+        double second = s.nextDouble();
         if(operation.equals("+"))
         {
             System.out.println("Answer: " + (first + second));
+        }
+        if(operation.equals("-"))
+        {
+            System.out.println("Answer: " + (first - second));
+        }
+        if(operation.equals("/"))
+        {
+            System.out.println("Answer: " + (first / second));
+        }
+        if(operation.equals("*"))
+        {
+            System.out.println("Answer: " + (first * second));
+        }
+        if(operation.equals("%"))
+        {
+            System.out.println("Answer: " + (first % second));
         }
         s.close();
     }
